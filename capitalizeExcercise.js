8afc525ac47fea18e8079be74801f25263740050
rmiller02 <r_miller02@yahoo.com>
2020-04-08 16:51:17 -0400

    first commit

---
+++ capitalizeExcercise.js
@@ -0,0 +1,5 @@
+var array = ['rza','gza','raekwon','method man', 'odb','ghostface killa']
+for (var i = 0; i < array.length; i++) {
+array[i] = array[i].toUpperCase();
+}
+console.log(array)
---
+++ pizzaexcercise.js
@@ -0,0 +1,16 @@
+var pizzaToppings = ['cheese', 'pepperoni', 'sauce','garlic', 'peppers' ];
+   
+   function myFunction() {
+    var myPizza = pizzaToppings.join(' and ')
+        return "A delicious pizza with " + myPizza;
+         
+    }
+
+console.log(myFunction());
+
+
+
+
+
+
+  
